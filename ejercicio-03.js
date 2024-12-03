6b0576fab72d953e517ad26fe1c68d3294acbfb1
alba_xiaoyang <albareyesconcepcion@gmail.com>
2024-12-03 21:01:58 +0000

    sacabo

--- ejercicio-03.js
+++ ejercicio-03.js
@@ -1,6 +1,6 @@
-function pedirValorJugador () {
+/*function pedirValorJugador () {
     var jugador = prompt ('Inserte: piedra/papel/tijeras'); 
-    var opciones = ["piedra", "papel", "tijera"]; 
+    var opciones = ["piedra", "papel", "tijeras"]; 
     var noValidacion = opciones.every (opcion => opcion!==jugador); 
     while (noValidacion) {
         jugador = prompt ('Inserte: piedra/papel/tijeras'); 
@@ -8,48 +8,41 @@ function pedirValorJugador () {
     }; 
     return jugador; 
 }
+    */
 
-function resultado (jugador1, jugador2) {
-    if (jugador1===jugador2) {
-        return ("Empate"); 
-    };
-    if ((jugador1=="piedra") && (jugador2=="lagarto")) {
-        return ("Jugador 1 gana!"); 
-    }; 
-    if ((jugador1=="tijeras") && (jugador2=="papel")) {
-        return ("Jugador 1 gana!"); 
-    }; 
-    if ((jugador1=="papel") && (jugador2=="piedra")) {
-        return ("Jugador 1 gana!"); 
+var jugador1="";
+var jugador2= ""; 
+
+function opcionEscogida(jugador, opcion){
+    alert(`El jugador ${jugador} escogio ${opcion}`)
+    if (jugador=="1") {
+        jugador1=opcion; 
     }
-    if ((jugador1=="lagarto") && (jugador2=="spock")) {
-        return ("Jugador 1 gana!"); 
+    else if (jugador=="2") {
+        jugador2=opcion; 
     }
-    if ((jugador1=="spock") && (jugador2=="tijeras")) {
-        return ("Jugador 1 gana!"); 
-    }
-    if ((jugador1=="tijeras") && (jugador2=="lagarto")) {
-        return ("Jugador 1 gana!"); 
-    }
-    if ((jugador1=="lagarto") && (jugador2=="papel")) {
-        return ("Jugador 1 gana!"); 
-    }
-    if ((jugador1=="papel") && (jugador2=="spock")) {
-        return ("Jugador 1 gana!"); 
-    }
-    if ((jugador1=="spock") && (jugador2=="piedra")) {
-        return ("Jugador 1 gana!"); 
-    }
-    if ((jugador1=="piedra") && (jugador2=="tijeras")) {
-        return ("Jugador 1 gana!"); 
-    }
-    return ("Jugador 2 gana!"); 
 }
 
-alert ("Te toca, jugador 1!"); 
+function resultado (alba, omar) {
+    if (alba===omar) {
+        return alert("Empate"); 
+    };
+    if ((alba=="tijeras") && (omar=="papel")) {
+        return alert("Jugador 1 gana!"); 
+    }; 
+    if ((alba=="papel") && (omar=="piedra")) {
+        return alert("Jugador 1 gana!"); 
+    }
+    if ((alba=="piedra") && (omar=="tijeras")) {
+        return alert("Jugador 1 gana!"); 
+    }
+    return alert("Jugador 2 gana!"); 
+}
+
+/*alert ("Te toca, jugador 1!");
 var jugador1 = pedirValorJugador (); 
 alert ("Te toca, jugador 2!");
 var jugador2 = pedirValorJugador (); 
 var mensaje = resultado (jugador1, jugador2); 
 
-alert (mensaje); 
+alert (mensaje); */
